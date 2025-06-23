503613dc751b8d1e38ad46685dc88ed53bad82d1
Igor <deputatov0@mail.ru>
2025-06-23 23:45:48 +0300

    +

--- dist/script.js
+++ dist/script.js
@@ -16,3 +16,23 @@ const state = {
     phase: 'work',
     intervalId: null
 };
+// проверка состояния старта
+function handleStart() {
+    if (state.intervalId || state.timeLeft <= 0)
+        return;
+    state.intervalId = setInterval(() => {
+        state.timeLeft--;
+        updateDisplay();
+        if (state.timeLeft <= 0) {
+            clearInterval(state.intervalId);
+            state.intervalId = null;
+        }
+    }, 1000);
+}
+// высчитывание секунд и минут с послед. преобразованием
+function updateDisplay() {
+    const minutes = Math.floor(state.timeLeft / 60).toString().padStart(2, '0');
+    const seconds = Math.floor(state.timeLeft % 60).toString().padStart(2, '0');
+    timer.textContent = `${minutes}:${seconds}`;
+}
+;
