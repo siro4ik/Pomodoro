4631c2fe8171ab560069a681c583096ac3ec7a40
Igor <deputatov0@mail.ru>
2025-06-25 00:31:24 +0300

    +

--- src/script.ts
+++ src/script.ts
@@ -4,19 +4,24 @@ const startBtn = document.querySelector('.start-btn') as HTMLElement;
 if (!startBtn) throw new Error ("Кнопка старта не обнаружена")
 const pauseBtn = document.querySelector('.pause-btn') as HTMLElement;
 if (!pauseBtn) throw new Error ("Кнопка паузы не обнаружена")
-const stopBtn = document.querySelector('.stop-btn') as HTMLElement;
-if (!stopBtn) throw new Error ("Кнопка стоп не обнаружена");
+const skipBtn = document.querySelector('.skip-btn') as HTMLElement;
+if (!skipBtn) throw new Error ("Кнопка пропуска не обнаружена");
+
+const WORK_TIME = 25 * 60;
+const BREAK_TIME = 5 * 60;
 
 type TimerState ={
     timeLeft: number;
     phase: 'work' | 'break';
     intervalId: number | null;
+    isPaused: true | false;
 };
 
 const state: TimerState = {
     timeLeft: 25 * 60,
     phase: 'work',
-    intervalId: null
+    intervalId: null,
+    isPaused: false
 };
 
 
@@ -36,6 +41,33 @@ function handleStart(){
     }, 1000);
 }
 
+// кнопка паузы, проверка состояния
+function handlePause(){
+
+    if(!state.intervalId) return;
+
+    clearInterval(state.intervalId);
+    state.intervalId = null;
+
+}
+
+// кнопка пропуска фазы
+function handleSkip(){
+
+    if(state.phase == 'break'){
+        state.phase = 'work';
+        state.timeLeft = WORK_TIME;
+    }else{
+        state.phase = 'break';
+        state.timeLeft = BREAK_TIME;
+    }
+
+    state.isPaused = false;
+    updateDisplay();
+
+
+}
+
 // высчитывание секунд и минут с послед. преобразованием
 function updateDisplay(){
     const minutes = Math.floor(state.timeLeft/60).toString().padStart(2, '0');
@@ -48,4 +80,4 @@ function updateDisplay(){
 
 startBtn.addEventListener('click', handleStart);
 pauseBtn.addEventListener('click', handlePause);
-stopBtn.addEventListener('click', handleStop);
+skipBtn.addEventListener('click', handleSkip);
